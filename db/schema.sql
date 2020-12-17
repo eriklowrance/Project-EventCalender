199ef68b09d7444eba1e5952d239fea297347813
Erik Lowrance <eriklowrance@gmail.com>
2020-12-17 11:26:50 -0700

    got auth token

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE twitchClips_db;
 
 USE twitchClips_db;
 
-CREATE TABLE clips(
+-- CREATE TABLE clips(
 
-  id INT NOT NULL AUTO_INCREMENT,
-  streamer_name VARCHAR(100) NOT NULL,
-  game VARCHAR(45) NOT NULL,
-  streamer_link VARCHAR(100) NOT NULL,
-  clip_link VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
+--   id INT NOT NULL AUTO_INCREMENT,
+--   streamer_name VARCHAR(100) NOT NULL,
+--   game VARCHAR(45) NOT NULL,
+--   streamer_link VARCHAR(100) NOT NULL,
+--   clip_link VARCHAR(100) NOT NULL,
+--   PRIMARY KEY (id)
   
-);
+-- );
